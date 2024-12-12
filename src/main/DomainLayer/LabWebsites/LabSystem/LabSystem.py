01bfefe3499b722a8ceeb0c2a2395592de41ff6a
assayagt <assayagt@post.bgu.ac.il>
2024-12-12 18:11:28 +0200

    Added some crawling functionality

--- src/main/DomainLayer/LabWebsites/LabSystem/LabSystem.py
+++ src/main/DomainLayer/LabWebsites/LabSystem/LabSystem.py
@@ -10,3 +10,17 @@ class LabSystem:
         if LabSystem._singleton_instance is None:
             LabSystem._singleton_instance = LabSystem()
         return LabSystem._singleton_instance
+
+    def crawl_for_publications(self):
+        """
+        Fetches publications for the given authors and year from all WebCrawlers.
+        """
+        #call the WebCrawlerFacade to fetch publications
+        WebCrawlerFacade().fetch_publications()
+
+        #TODO: Send notifications to the website members about the new publications
+
+
+
+
+
--- src/main/DomainLayer/LabWebsites/Website/WebsiteFacade.py
+++ src/main/DomainLayer/LabWebsites/Website/WebsiteFacade.py
@@ -13,3 +13,23 @@ class WebsiteFacade:
 
     def get_all_websites(self):
         return self.websites
+
+    def crawl_for_publications(self):
+        # get list of all websites
+        websites = get_all_websites()
+
+        # for each website, send to the webCrawler facade the members and current year to fetch publications
+        publications = []
+        for website in websites:
+            publications.extend(WebCrawlerFacade().fetch_publications(website.members, datetime.now().year))
+
+            # check that each publication is not already in website members publications
+            for publication in publications:
+                if website.check_publication_exist(publication):
+                    publications.remove(publication)
+
+
+            #TODO : After sending the notifications to the website members, add the new publications to the website
+            # add the new publications to the website
+            # for publication in publications:
+            #     website.create_publication(publication.title, publication.authors, publication.date, publication.approved, publication.publication_link, publication.media)
