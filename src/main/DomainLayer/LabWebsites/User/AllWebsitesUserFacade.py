eb66168d020dea5894f6ea5107c663933fee79ca
Nitzan Horovitz <horovitn@post.bgu.ac.il>
2024-12-21 16:08:34 +0200

    Added some changes

--- src/main/DomainLayer/LabWebsites/User/AllWebsitesUserFacade.py
+++ src/main/DomainLayer/LabWebsites/User/AllWebsitesUserFacade.py
@@ -17,17 +17,26 @@ class AllWebsitesUserFacade:
         userFacade = self.getUserFacadeByDomain(domain)
         userFacade.logout(userId)
 
-    def create_new_site_manager(self, nominator_manager_userId, nominated_manager_email, domain):
+    def create_new_site_manager_from_labWebsite(self, nominator_manager_userId, nominated_manager_email, domain):
         userFacade = self.getUserFacadeByDomain(domain)
         userFacade.error_if_user_notExist(nominator_manager_userId)
         userFacade.error_if_user_not_logged_in(nominator_manager_userId)
         userFacade.error_if_labMember_notExist(nominated_manager_email)
         userFacade.create_new_site_manager(nominated_manager_email)
 
-    def register_new_LabMember(self, manager_userId, email_to_register, domain):
+    def register_new_LabMember_from_labWebsite(self, manager_userId, email_to_register, domain):
         userFacade = self.getUserFacadeByDomain(domain)
         userFacade.error_if_user_notExist(manager_userId)
         userFacade.error_if_user_not_logged_in(manager_userId)
+        userFacade.register_new_LabMember(email_to_register)
+
+    def create_new_site_manager_from_generator(self, nominated_manager_email, domain):
+        userFacade = self.getUserFacadeByDomain(domain)
+        userFacade.error_if_labMember_notExist(nominated_manager_email)
+        userFacade.create_new_site_manager(nominated_manager_email)
+
+    def register_new_LabMember_from_generator(self, email_to_register, domain):
+        userFacade = self.getUserFacadeByDomain(domain)
         userFacade.register_new_LabMember(email_to_register)
 
     def getMemberEmailByName(self, author, domain):
