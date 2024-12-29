8ba7958ade4fb3061ca31ff75dd29a2ba4b63bf2
Nitzan Horovitz <horovitn@post.bgu.ac.il>
2024-12-29 10:56:40 +0200

    Added functions for inital + final approve publications

--- src/main/Util/ExceptionsEnum.py
+++ src/main/Util/ExceptionsEnum.py
@@ -13,3 +13,4 @@ class ExceptionsEnum(Enum):
     USER_IS_NOT_A_LAB_MEMBER_OR_LAB_MANAGER_OR_CREATOR = "User is not a lab member or a lab manager or a site creator"
     USER_IS_NOT_A_LAB_MANAGER = "User is not a lab manager"
     USER_IS_NOT_PUBLICATION_AUTHOR_OR_LAB_MANAGER = "User is not a publication author or a lab manager"
+    PUBLICATION_ALREADY_APPROVED = "Publication already approved, and appears on the website"
