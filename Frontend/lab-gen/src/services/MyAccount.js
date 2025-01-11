a1d7e64b5fbf0faea3d909211005f1e2258d63af
lizetpo <lizetpo@post.bgu.ac.il>
2025-01-11 14:56:50 +0200

    ADDED CALSS

---
+++ Frontend/lab-gen/src/services/MyAccount.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const baseApiUrl = "http://127.0.0.1:5000/api/";
+
+
+
+export const getCustomWebsites = async (userId) => {
+    try {
+        const response = await axios.get(`${baseApiUrl}getCustomWebsites`, { params: { user_id: userId } });
+        return response.data.websites;
+    } catch (error) {
+        console.error("Error fetching custom websites:", error);
+        return [];
+    }
+};
+
+export const getCustomSite = async (userId, domain) => {
+    try {
+        const response = await axios.get(`${baseApiUrl}getCustomSite`, { params: { user_id: userId, domain } });
+        return response.data.data;
+    } catch (error) {
+        console.error("Error fetching custom site details:", error);
+        return null;
+    }
+};
--- Frontend/template1/services/websiteService.js
+++ Frontend/template1/services/websiteService.js
@@ -32,25 +32,7 @@ export const addLabManager = async (userId, email, domain) => {
     }
 };
 
-export const getCustomWebsites = async (userId) => {
-    try {
-        const response = await axios.get(`${baseApiUrl}getCustomWebsites`, { params: { user_id: userId } });
-        return response.data.websites;
-    } catch (error) {
-        console.error("Error fetching custom websites:", error);
-        return [];
-    }
-};
 
-export const getCustomSite = async (userId, domain) => {
-    try {
-        const response = await axios.get(`${baseApiUrl}getCustomSite`, { params: { user_id: userId, domain } });
-        return response.data.data;
-    } catch (error) {
-        console.error("Error fetching custom site details:", error);
-        return null;
-    }
-};
 
 export const approveRegistration = async (data) => {
     try {
@@ -200,3 +182,5 @@ export const fetchAllLabWebsites = async (userId) => {
         return [];
     }
 };
+
+
