3b049ca3b226be3f5ba47546b9ca0b76beddd821
lizetpo <lizetpo@post.bgu.ac.il>
2025-01-14 14:02:12 +0200

    added small change

--- Frontend/template1/src/Pages/HomePage/HomePage.jsx
+++ Frontend/template1/src/Pages/HomePage/HomePage.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import "./HomePage.css";
 import AboutUs from "../../Components/AboutUs/AboutUs";
+import { useAuth } from "../../Context/AuthContext";
 
 
 function HomePage(props) {
-
+  
+  const { fetchToken } = useAuth();
+  function fetchData() {
+    const storedSid = sessionStorage.getItem('sid');
+    if (!storedSid) {
+      fetchToken();  
+    }    
+  }
 
   return (
       <div className='HomePage'>
+          {fetchData()}
         <div className="main_section_homePage">
             <div className="subTitle">Welcome to our lab website.<br/> This is where there will be a custom welcome message</div>
             <div className="homeImg">to be img</div>
