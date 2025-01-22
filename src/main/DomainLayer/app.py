ea655cb50b61a12c7f3879732c17a0e61a2ed640
assayagt <assayagt@post.bgu.ac.il>
2025-01-22 15:31:43 +0200

    Changed app.py again

--- src/main/DomainLayer/app.py
+++ src/main/DomainLayer/app.py
@@ -1215,7 +1215,7 @@ api.add_resource( GetContactUs, '/api/getContactUs')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
 
 
 def helper():
