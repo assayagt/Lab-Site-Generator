5609f867cd04db7468459e931345881b9ca8fd3d
lizetpo <lizetpo@post.bgu.ac.il>
2025-01-24 20:39:29 +0200

    added more design changes

--- Frontend/template1/src/Components/AddPublicationForm/AddPubliactionForm.jsx
+++ Frontend/template1/src/Components/AddPublicationForm/AddPubliactionForm.jsx
@@ -34,43 +34,50 @@ const AddPublicationForm = () => {
     <div className="upload-publication-section">
       <h3>Add New Publication</h3>
       <form className="upload-publication-form" onSubmit={handleSubmit}>
-        <label>
-          <strong>publication-link:</strong>
+        <div className="coolinput">
+          <label htmlFor="input" className="text">Publication Link:</label>
           <input
             type="text"
+            placeholder="publication link"
+            name="input"
+            className="input"
             value={publication}
             onChange={(e) => setPublication(e.target.value)}
-            placeholder="Publication link"
-            required
           />
-        </label>
-        <label>
-          <strong>GitHub Link:</strong>
+        </div>
+        <div className="coolinput">
+          <label htmlFor="input" className="text">GitHub Link:</label>
           <input
-            type="url"
+            type="text"
+            placeholder="github link"
+            name="input"
+            className="input"
             value={githubLink}
             onChange={(e) => setGithubLink(e.target.value)}
-            placeholder="GitHub URL"
           />
-        </label>
-        <label>
-          <strong>Presentation Link:</strong>
+        </div>
+        <div className="coolinput">
+          <label htmlFor="input" className="text">Presentation Link:</label>
           <input
-            type="url"
+            type="text"
+            placeholder="presentation link"
+            name="input"
+            className="input"
             value={presentationLink}
             onChange={(e) => setPresentationLink(e.target.value)}
-            placeholder="Presentation URL"
           />
-        </label>
-        <label>
-          <strong>Video Link:</strong>
+        </div>
+        <div className="coolinput">
+          <label htmlFor="input" className="text">Video Link:</label>
           <input
-            type="url"
+            type="text"
+            placeholder="video link"
+            name="input"
+            className="input"
             value={videoLink}
             onChange={(e) => setVideoLink(e.target.value)}
-            placeholder="Video URL"
           />
-        </label>
+        </div>
         <button type="submit">Add Publication</button>
       </form>
     </div>
