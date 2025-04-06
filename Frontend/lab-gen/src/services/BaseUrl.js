55a90031d6c62d204fb6fecd764a58089371ad3d
lizetpo <lizetpo@post.bgu.ac.il>
2025-04-06 14:41:05 +0300

    added new adress

--- Frontend/lab-gen/src/services/BaseUrl.js
+++ Frontend/lab-gen/src/services/BaseUrl.js
@@ -1,1 +1,3 @@
-export const baseApiUrl = "http://127.0.0.1:5000/api/";
+// export const baseApiUrl = "http://127.0.0.1:5000/api/";
+
+export const baseApiUrl = "http://132.72.116.69:5000/api/";
--- Frontend/template1/src/services/BaseUrl.js
+++ Frontend/template1/src/services/BaseUrl.js
@@ -1,1 +1,2 @@
-export const baseApiUrl = "http://127.0.0.1:5000/api/";
+// export const baseApiUrl = "http://127.0.0.1:5000/api/";
+export const baseApiUrl = "http://132.72.116.69:5000/api/";
