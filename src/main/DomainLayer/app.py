fce6fe9fa449eab361eae03fe7ae6557f96c7610
assayagt <assayagt@post.bgu.ac.il>
2025-04-27 16:07:51 +0300

    Fixed socketio

--- src/main/DomainLayer/app.py
+++ src/main/DomainLayer/app.py
@@ -9,7 +9,7 @@ import subprocess
 import pandas as pd
 from flask_socketio import SocketIO, emit
 import threading
-from src.main.DomainLayer.socketio_instance import init_socketio, socketio
+from src.main.DomainLayer.socketio_instance import init_socketio
 import shutil
 
 def send_test_notifications():
@@ -33,7 +33,7 @@ app.config["SECRET_KEY"] = app_secret_key
 
 CORS(app, resources={r"/*": {"origins": ["http://lsg.cs.bgu.ac.il"]}})
 api = Api(app)
-init_socketio(app)
+socketio = init_socketio(app)
 # Directories for file storage and website generation
 UPLOAD_FOLDER = './uploads'
 GENERATED_WEBSITES_FOLDER = './LabWebsitesUploads'
--- src/main/DomainLayer/socketio_instance.py
+++ src/main/DomainLayer/socketio_instance.py
@@ -1,3 +1,4 @@
+# socketio_instance.py
 from flask_socketio import SocketIO
 
 socketio = None
@@ -5,3 +6,4 @@ socketio = None
 def init_socketio(app):
     global socketio
     socketio = SocketIO(app, cors_allowed_origins=["http://lsg.cs.bgu.ac.il"], async_mode="gevent")
+    return socketio
